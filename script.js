f06b58b083305fbebd188a3866bfc18c65efaf05
PhredE <phrede@PhredEs-Air.lan1>
2022-07-22 14:29:47 -0700

    2nd commit

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-alert('whats bracking?')
+//document.getElementById("count").innerText = 5
+//let firstBatch = 5
+//let secondBatch = 7
+//let count = firstBatch + secondBatch
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result of myDogAge
+// 3. Log myDogAge to the console 
+
+// let myAge = 30
+// let humanDogRatio = 7
+// let myDogAge = humanDogRatio * myAge
+
+// console.log(myDogAge)
+
+// let count = 5
+// //count + 1
+
+// count = count + 1
+
+// console.log(count)
+
+// let bonusPoints = 50
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+function increment() {
+    
+    console.log("the button was clicked")
+}
